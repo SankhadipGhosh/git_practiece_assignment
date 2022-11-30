d68164f9e6ebccf53e870448615edced0ae8d166
SankhadipGhosh <sayanghosh281@gmail.com>
2022-12-01 01:52:23 +0530

    problem1 done

---
+++ problem1.js
@@ -0,0 +1,10 @@
+//if a number is prime or not
+
+
+let num=17;
+let c=0;
+   for(i=2;i<num;i++)
+     {if (nm%i===0) {c=c+1} }
+      
+      if(c===0) {console.log("Yes it is a prime")}
+       else {console.log("No it is not a prime")
